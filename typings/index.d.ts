086610e9f4de37a8f7ab6d346116562164670da1
SpeckyYT <Specky.fans@gmail.com>
2020-07-19 22:13:25 +0200

    fixed typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,28 +1,25 @@
 declare class WaifuLabs {
     constructor();
     generateWaifus():Promise<Waifus>;
-    generateBigWaifu(data:Waifu|Seeds):Promise<Waifu>;
-    generateProduct(data:Waifu|Seeds, product:"PILLOW"|"POSTER"):Promise<Waifu>;
+    generateWaifus(data: Waifu|Seeds, step: 0|1|2|3):Promise<Waifus>;
+    generateBigWaifu(data: Waifu|Seeds):Promise<Waifu>;
+    generateProduct(data: Waifu|Seeds, product: "PILLOW" | "POSTER"):Promise<Waifu>;
 }
-
 type Seeds = [
     Number, Number, Number, Number,
     Number, Number, Number, Number,
     Number, Number, Number, Number,
     Number, Number, Number, Number,
     Number, Array<Number>
-]
-
+];
 type Waifu = {
     image: String;
     seeds: Seeds;
-}
-
+};
 type Waifus = [
     Waifu, Waifu, Waifu, Waifu,
     Waifu, Waifu, Waifu, Waifu,
     Waifu, Waifu, Waifu, Waifu,
     Waifu, Waifu, Waifu, Waifu
-]
-
-export = WaifuLabs
+];
+export = WaifuLabs;
