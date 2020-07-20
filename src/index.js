11e7874dec2fb7b5b05d1bebcc2f5e6a3c2d64be
SpeckyYT <Specky.fans@gmail.com>
2020-07-20 10:34:00 +0200

    minor code change

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const fetchWaifuLabs = require('./fetch');
+const fetch = require('./fetch');
 const errorUnex = "Unexpected Error occurred (parameters may be wrong)";
 const errorSeeds = "No valid Waifu or Seeds provided";
 
@@ -6,16 +6,16 @@ class WaifuLabs {
     constructor(){
         this.generateWaifus = async (data, step) => {
             let object = {};
-            object.step = Math.max(0, Math.min(3, step || 0));
+            object.step = Math.max(0, Math.min(3, step)) || 0;
             if(object.step > 0) object.currentGirl = data.seeds || data;
-            return fetchWaifuLabs('generate', object)
+            return fetch('generate', object)
             .then(r => r.newGirls)
             .catch(() => new Error(errorUnex));
         };
         this.generateBigWaifu = async (data) => {
             const seeds = data.seeds || data;
             if(!seeds) throw new Error(errorSeeds);
-            return fetchWaifuLabs('generate_big', {currentGirl:seeds})
+            return fetch('generate_big', {currentGirl:seeds})
             .then(r => Object({image:r.girl,seeds:seeds}))
             .catch(() => new Error(errorUnex));
         };
@@ -24,7 +24,7 @@ class WaifuLabs {
             if(!seeds) throw new Error(errorSeeds);
             let _product = (typeof product == 'string' ? product : '').toUpperCase();
             if(!['PILLOW','POSTER'].includes(_product)) _product = 'POSTER';
-            return fetchWaifuLabs('generate_preview', {currentGirl:seeds,product:_product})
+            return fetch('generate_preview', {currentGirl:seeds,product:_product})
             .then(r => Object({image:r.girl,seeds:seeds}))
             .catch(() => new Error(errorUnex));
         };
