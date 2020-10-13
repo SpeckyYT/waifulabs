fa9ddc595204aaf31342cfdeaa8044a1d7945fae
SpeckyYT <Specky.fans@gmail.com>
2020-10-13 21:35:43 +0200

    added node-fetch module

--- src/fetch.js
+++ src/fetch.js
@@ -1,28 +1,12 @@
-const https = require('https');
+const fetch = require('node-fetch');
+
+const protocolURL = 'https://';
 const baseURL = "api.waifulabs.com";
 
-module.exports = (endpoint, _data) => {
-    const data = JSON.stringify(_data);
-    return new Promise((res,rej) => {
-        const cr = https.request({
-            host: baseURL,
-            path: endpoint,
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': data.length
-            }
-        }, r => {
-            if(Math.floor(r.statusCode/100) != 2) rej();
-            let collected = '';
-            r.on("data", c => {
-                collected += c;
-            });
-            r.on("close", () => {
-                res(JSON.parse(collected));
-            });
-        });
-        cr.write(data);
-        cr.end();
+module.exports = (endpoint, data) => {
+    return fetch(`${protocolURL}${baseURL}/${endpoint}`, {
+        method: 'POST',
+        body: JSON.stringify(data)
     })
+    .then(resp => resp.json());
 }
