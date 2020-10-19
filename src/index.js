0cebf2ddafb72b48ac8ce156a2e22e32cb9febf5
SpeckyYT <Specky.fans@gmail.com>
2020-10-19 22:24:31 +0200

    cleaned up the code a bit

--- src/index.js
+++ src/index.js
@@ -1,32 +1,51 @@
 const fetch = require('./fetch');
 const isSeed = require('./isValidSeed');
+
 const errorUnex = "Unexpected Error occurred (parameters may be wrong)";
 const errorSeeds = "No valid Waifu or Seeds provided";
 
 const WaifuLabs = {
-    generateWaifus: async (data, step) => {
+    async generateWaifus (data, step) {
+
         let object = {};
+
         object.step = Math.max(0, Math.min(3, step)) || 0;
-        if(object.step > 0) object.currentGirl = data.seeds || data;
+
+        if(object.step > 0){
+            object.currentGirl = data.seeds || data
+            if(!isSeed(object.currentGirl)) throw new TypeError(errorSeeds);
+        }
+
         return fetch('generate', object)
         .then(r => r.newGirls)
-        .catch(() => new Error(errorUnex));
+        .catch(() => {throw new Error(errorUnex)});
+
     },
-    generateBigWaifu: async (data) => {
+
+    async generateBigWaifu (data) {
+
         const seeds = data.seeds || data;
-        if(!isSeed(seeds)) throw new Error(errorSeeds);
+        if(!isSeed(seeds)) throw new TypeError(errorSeeds);
+
         return fetch('generate_big', {currentGirl:seeds})
         .then(r => Object({image:r.girl,seeds:seeds}))
-        .catch(() => new Error(errorUnex));
+        .catch(() => {throw new Error(errorUnex)});
+
     },
-    generateProduct: async (data, product) => {
+
+    async generateProduct (data, product) {
+
         const seeds = data.seeds || data;
-        if(!isSeed(seeds)) throw new Error(errorSeeds);
+        if(!isSeed(seeds)) throw new TypeError(errorSeeds);
+
         let _product = (typeof product == 'string' ? product : '').toUpperCase();
+
         if(!['PILLOW','POSTER'].includes(_product)) _product = 'PILLOW';
+
         return fetch('generate_preview', {currentGirl:seeds,product:_product})
         .then(r => Object({image:r.girl,seeds:seeds}))
-        .catch(() => new Error(errorUnex));
+        .catch(() => {throw new Error(errorUnex)});
+        
     }
 }
 
