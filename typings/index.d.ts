aaaf301cef134ee7e0c147d03acdfa08beb0db7b
SpeckyYT <Specky.fans@gmail.com>
2021-01-22 19:27:04 +0100

    fixed typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,9 +1,3 @@
-type WaifuLabs = {
-    generateWaifus():Promise<Waifus>;
-    generateWaifus(data: Waifu|Seeds, step: 0|1|2|3):Promise<Waifus>;
-    generateBigWaifu(data: Waifu|Seeds):Promise<Waifu>;
-    generateProduct(data: Waifu|Seeds, product: "PILLOW" | "POSTER"):Promise<Waifu>;
-}
 type Seeds = [
     Number, Number, Number, Number,
     Number, Number, Number, Number,
@@ -21,4 +15,8 @@ type Waifus = [
     Waifu, Waifu, Waifu, Waifu,
     Waifu, Waifu, Waifu, Waifu
 ];
-export = WaifuLabs;
+export function generateWaifus():Promise<Waifus>;
+export function generateWaifus(data: Waifu|Seeds, step: 0|1|2|3):Promise<Waifus>;
+export function generateBigWaifu(data: Waifu|Seeds):Promise<Waifu>;
+export function generateProduct(data: Waifu|Seeds, product: "PILLOW" | "POSTER"):Promise<Waifu>;
+export function isValidSeed(seeds: any):Boolean;
