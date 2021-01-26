513c7b8ba1fd079a7466a613458c4f0c066eb819
SpeckyYT <Specky.fans@gmail.com>
2021-01-26 21:19:39 +0100

    made huge rework on the code

--- src/index.js
+++ src/index.js
@@ -5,62 +5,83 @@ const errorSeeds = "No valid Waifu or Seeds provided";
 
 const WaifuLabs = {
     async generateWaifus (data, step) {
-
-        const object = {};
-
-        object.step = Math.max(0, Math.min(3, Math.floor(step))) || 0;
-
+        const object = {
+            step: resolveStep(step)
+        };
         if(object.step > 0){
-            object.currentGirl = data.seeds || data
+            object.currentGirl = resolveWaifu(data);
             if(!this.isValidSeed(object.currentGirl)) throw new TypeError(errorSeeds);
         }
 
         return fetch('generate', object)
         .then(r => r.newGirls)
         .catch(() => {throw new Error(errorUnex)});
-
     },
 
     async generateBigWaifu (data) {
-
-        const seeds = data.seeds || data;
+        const seeds = resolveWaifu(data);
         if(!this.isValidSeed(seeds)) throw new TypeError(errorSeeds);
 
         return fetch('generate_big', {currentGirl:seeds})
-        .then(r => Object({image:r.girl,seeds:seeds}))
+        .then(r => ({image:r.girl,seeds:seeds}))
         .catch(() => {throw new Error(errorUnex)});
-
     },
 
     async generateProduct (data, product) {
-
-        const seeds = data.seeds || data;
+        const seeds = resolveWaifu(data);
         if(!this.isValidSeed(seeds)) throw new TypeError(errorSeeds);
 
-        let _product = (typeof product == 'string' ? product : '').toUpperCase();
-
-        if(!['PILLOW','POSTER'].includes(_product)) _product = 'PILLOW';
+        const _product = resolveProduct(product);
 
         return fetch('generate_preview', {currentGirl:seeds,product:_product})
-        .then(r => Object({image:r.girl,seeds:seeds}))
+        .then(r => ({image:r.girl,seeds:seeds}))
         .catch(() => {throw new Error(errorUnex)});
-        
     },
 
     isValidSeed (seeds) {
         if(!Array.isArray(seeds)) return false;
         if(seeds.length < 17) return false;
-        if(
-            !seeds.slice(0,16).some(seed =>
-                isNaN(seed) ||
-                !Number.isInteger(seed) ||
-                seed < 0 ||
-                seed > 2**32-1
-            )
-        ) return true;
-        return false;
+        return seeds.slice(0,16).every(seed => {
+            if(isNaN(seed)) return false;
+            if(!Number.isInteger(seed)) return false;
+            if(seed < 0) return false;
+            if(seed >= 2**32) return false;
+            return true;
+        });
     }
-    
+}
+
+function resolveWaifu (waifu) {
+    if(!waifu) return;
+    if(Array.isArray(waifu)) return waifu;
+    if(typeof waifu == 'object') return resolveWaifu(waifu.seeds); 
+}
+
+const steps = {
+    base: 0,
+    color: 1,
+    details: 2,
+    pose: 3
+}
+function resolveStep (step) {
+    if(typeof step == 'number'){
+        if(Object.values(steps).includes(step)) return step;
+    }else if(typeof step == 'string'){
+        if(typeof steps[step] != 'undefined') return steps[step];
+    }
+    return 0;
+}
+
+const products = [
+    'PILLOW',
+    'POSTER'
+]
+function resolveProduct (product) {
+    if(typeof product == 'string'){
+        const prod = product.toUpperCase();
+        if(products.includes(prod)) return prod;
+    }
+    return products[0];
 }
 
 module.exports = WaifuLabs;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -15,8 +15,9 @@ type Waifus = [
     Waifu, Waifu, Waifu, Waifu,
     Waifu, Waifu, Waifu, Waifu
 ];
+type Steps = 0 | 1 | 2 | 3 | 'base' | 'color' | 'details' | 'pose';
 export function generateWaifus():Promise<Waifus>;
-export function generateWaifus(data: Waifu|Seeds, step: 0|1|2|3):Promise<Waifus>;
+export function generateWaifus(data: Waifu|Seeds, step: Steps):Promise<Waifus>;
 export function generateBigWaifu(data: Waifu|Seeds):Promise<Waifu>;
 export function generateProduct(data: Waifu|Seeds, product: "PILLOW" | "POSTER"):Promise<Waifu>;
 export function isValidSeed(seeds: any):Boolean;
